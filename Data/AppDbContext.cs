e0e850f0620db93ffec3b730e861474a972b12f6
ReimannHeiliger <tomas.reimann@ce.ucn.cl>
2025-04-16 16:51:23 -0400

    initialization

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Conserge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conserge.Data;
+
+/// <summary>
+/// The Application Database Context.
+/// </summary>
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        // empty
+    }
+    
+    /// <summary>
+    /// The Personas DbSet.
+    /// </summary>
+    public DbSet<Persona> Personas => Set<Persona>();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="modelBuilder"></param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // Configura the Persona entity
+        modelBuilder.Entity<Persona>();
+        
+        // Seed the database with initial data
+        modelBuilder.Entity<Persona>().HasData(
+            new Persona {Nombre = "Tomas", Apellidos = "Reimann", Rut = "196243232", Email = "tomas@gmail.com"}
+        );
+    }
+}
---
+++ Repositories/IPersonaRepository.cs
@@ -0,0 +1,30 @@
+﻿using Conserge.Models;
+
+namespace Conserge.Repositories;
+
+/// <summary>
+/// IRepository: Persona.
+/// </summary>
+public interface IPersonaRepository
+{
+    /// <summary>
+    /// Add a Persona into the Repository.
+    /// </summary>
+    /// <param name="persona"></param>
+    Task<Persona> Add(Persona persona);
+    
+    /// <summary>
+    /// Retrieve all the Personas from the Repository
+    /// </summary>
+    Task<IEnumerable<Persona>> GetAll(); 
+    
+    /// <summary>
+    ///  Retrieve a Persona by its ID
+    /// </summary>
+    Task<Persona?> GetById(int id);
+    
+    /// <summary>
+    ///  Retrieve a Persona by its email
+    /// </summary>
+    Task<Persona?> GetByEmail(string email);
+}
